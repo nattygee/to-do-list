25f81c8a72d1a8e540f84a51d8819f0cb8596e2f
nattygee <83288489+nattygee@users.noreply.github.com>
2022-02-25 23:14:52 -0500

    not finished but currently figuring how to create and displaying elements

--- scripts/toDoScript.js
+++ scripts/toDoScript.js
@@ -2,13 +2,33 @@ var taskTitle = document.getElementById('taskTitle');
 var taskDescription = document.getElementById('taskDescription');
 var submitBtn = document.getElementById('primaryCTA');
 
+var taskListDiv = document.getElementById('theListSide');
 var taskTitleSent = document.getElementById('titleSentInput');
 var taskDescriptionSent = document.getElementById('descriptionSentInput');
 
 submitBtn.addEventListener('click', () => {
+    createTask();
     sendTaskOver();
 });
 
+    //🏁 creating a brand new task from scratch in the task column
+function createTask() {
+    // creating a new element
+    let newTaskTitle = document.createElement('div');
+
+    // defining content to attach to the new element
+    let taskTitleContent = document.createTextNode(taskTitle.value);
+    
+    // actually attaching that content to the new element
+    newTaskTitle.appendChild(taskTitleContent);
+
+    // placing newly created div on the actual page
+    const parentDiv = taskListDiv.parentNode;
+    parentDiv.insertBefore(newTaskTitle, taskListDiv);
+
+
+    console.log(newTaskText);
+}
 
 function sendTaskOver() {
     //translate task input text to task list div
